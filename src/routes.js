353e8fbf2b8a43cad4899d7e8b433774565653d5
Kened Oliveira <kenedwanderson@live.com>
2019-08-31 17:52:07 -0300

    step#2/Interactions with database
    Create user model, controller, database migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import userController from './app/controllers/userController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   res.json({ ok: 'Aplication simple' });
 });
+
+routes.post('/users', userController.store);
+routes.put('/users', userController.update);
 
 export default routes;
